e37e1093df4e89ead234efc108627a703f600397
EdwardNickless <133578443+EdwardNickless@users.noreply.github.com>
2023-08-20 11:00:47 +0100

    Reorganized Project Structure
    - Moved Building.CS and all of it's child classes to Scripts/Buildings
    - Renamed MenuUIHanlder.cs to UIMenuScene.cs
    - Moved UIMenuScene.cs to Scripts/UI

--- Assets/Scripts/UI/UIMenuScene.cs
+++ Assets/Scripts/UI/UIMenuScene.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -11,7 +8,7 @@ using UnityEditor;
 // Sets the script to be executed later than all default scripts
 // This is helpful for UI, since other things may need to be initialized before setting the UI
 [DefaultExecutionOrder(1000)]
-public class MenuUIHandler : MonoBehaviour
+public class UIMenuScene : MonoBehaviour
 {
     public ColorPicker ColorPicker;
 
