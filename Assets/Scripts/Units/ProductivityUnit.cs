699c5e690bc53fccc2d58c1a756b0bd0179d6787
EdwardNickless <133578443+EdwardNickless@users.noreply.github.com>
2023-08-20 10:50:32 +0100

    Added Production rate increase functionality
    - Override method created for BuildingInRange() [multiplies Productivity Rate of ResoucrePile by ProductivityMultiplier]
    - Created ResetProductivity() method to return productivity rate of ResourcePile to original rate
    - Override methods that call ResetProductivity() created for:
      -- GotTo(Building)
      -- GotTo(Vector3)
    - Removed unneeded using statements from UserControl.cs

--- Assets/Scripts/Units/ProductivityUnit.cs
+++ Assets/Scripts/Units/ProductivityUnit.cs
@@ -2,8 +2,42 @@ using UnityEngine;
 
 public class ProductivityUnit : Unit
 {
+    public float ProductivityMultiplier = 2.0f;
+    
+    private ResourcePile m_currentPile = null;
+
     protected override void BuildingInRange()
     {
-        
+        if (m_currentPile == null)
+        {
+            ResourcePile pile = m_Target as ResourcePile;
+
+            if (pile != null)
+            {
+                m_currentPile = pile;
+                m_currentPile.ProductionSpeed *= ProductivityMultiplier;
+            }
+        }
+    }
+
+    private void ResetProductivity()
+    {
+        if (m_currentPile != null)
+        {
+            m_currentPile.ProductionSpeed /= ProductivityMultiplier;
+            m_currentPile = null;
+        }
+    }
+
+    public override void GoTo(Building target)
+    {
+        ResetProductivity();
+        base.GoTo(target);
+    }
+
+    public override void GoTo(Vector3 positon)
+    {
+        ResetProductivity();
+        base.GoTo(positon);
     }
 }
